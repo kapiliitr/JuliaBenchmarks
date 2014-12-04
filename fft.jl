fbb80af2677b3e3e4df46b1ec8a1993bb1977dcb
Kanu Sahai <ksahai3@jinx-login.cc.gatech.edu>
2014-12-04 15:03:09 -0500

    FFT implemented

---
+++ fft.jl
@@ -0,0 +1,127 @@
+A=[1,2,1,1,2,1,3,4]
+B=zeros(Complex,length(A))
+@everywhere N=length(A)
+P=4
+addprocs(P)
+
+function getBit(x,i)
+	z= convert(Uint64,x)>>convert(Uint64,i)
+        y=z&1
+        return y
+end
+
+function reverseBits(x,N)
+	for i=0:(N/2)-1
+        	if(getBit(x,i)!=getBit(x,N-1-i))
+                	y=(convert(Uint64,1)<<convert(Uint64,i))|(convert(Uint64,1)<<convert(Uint64,(N-1-i)))
+                   	x=convert(Uint64,x)$y
+               	end
+        end
+        x
+end
+
+for i=0:nprocs()-2
+        X=Array((Number,Number),convert(Integer,N/(nprocs()-1)))
+       	count=1
+        for j=i*(N/(nprocs()-1)):(i+1)*(N/(nprocs()-1))-1
+                X[count]=(reverseBits(j,log2(N)),convert(Complex,A[reverseBits(j,log2(N))+1]))
+                count+=1
+        end   
+        Y=sort(X)
+        count=1
+        for j=i*(N/(nprocs()-1)):(i+1)*(N/(nprocs()-1))-1
+              	B[j+1]=Y[count][2]
+               	count+=1
+        end
+end
+
+D=distribute(B)
+
+@everywhere function localComputation(D::DArray)
+	A=fft(localpart(D))
+	localpart(D)[1]=A[1]
+        for i=length(localpart(D)):-1:2
+        	localpart(D)[i]=A[length(localpart(D))-i+2]
+        end
+
+	
+end
+
+ref=[@spawnat p localComputation(D) for p in procs(D)]
+Aux=zeros(Complex,N)
+Aux=distribute(Aux)
+
+@everywhere function fetch_data(D::DArray)
+        return localpart(D)
+end
+
+
+@everywhere nthroots(n::Integer) = [ cospi(2k/n)+sinpi(2k/n)im for k = 0:n-1 ]
+
+@everywhere function parallel_fft(D::DArray, r, Aux::DArray)
+	#Proc to fetch data from
+	pos=(myid()-2)%(2^r)
+	if(pos<2^(r-1))
+		proc_num=myid()+2^(r-1)
+	else
+		proc_num=myid()-2^(r-1)
+	end
+	B=fetch(@spawnat proc_num fetch_data(D))
+	
+	#Creating the corresponding roots of unity
+	start_index=((myid()-2)*length(localpart(D)))%convert(Integer,(length(localpart(D))*(2^r))) +1
+	Roots=nthroots(convert(Integer,length(localpart(D))*(2^r)))[start_index:(start_index+length(localpart(D))-1)]
+	
+	if(proc_num>myid())
+		for i = 1:length(localpart(D))
+			localpart(Aux)[i]=localpart(D)[i]+Roots[i]*B[i]
+		end	
+	else
+		for i = 1:length(localpart(D))
+			localpart(Aux)[i]=B[i]+Roots[i]*localpart(D)[i]
+		end
+	end	
+end
+
+@everywhere function copy_array(D::DArray, Aux::DArray)
+	for i =1:length(localpart(D))
+		localpart(D)[i]=localpart(Aux)[i]
+	end
+end
+
+for r=1:convert(Integer,log2(P))
+
+	@sync {@spawnat p parallel_fft(D,r,Aux) for p in procs(D)}
+
+	@sync {@spawnat p copy_array(D,Aux) for p in procs(D)}
+
+end
+
+function verify()
+	seq_fft=fft(A)
+	seq_fft=[seq_fft[1],reverse(seq_fft[2:end])]
+
+	par_fft=convert(Array,D)
+
+	if(length(par_fft)!=length(seq_fft))
+		println("ERROR Detected : Size of arrays don't match")
+	else
+		flag=0
+		for i = 1:length(par_fft)
+			if(abs(par_fft[i]-seq_fft[i])>0.01)
+				println("WRONG Result")
+				println(seq_fft)
+				println(par_fft)
+				flag=1
+				break
+			end
+		end
+
+		if(flag==0)
+			println("GOOD Job!")
+		end
+	end
+
+end
+
+verify()
